79a36e9c60ffd029bcf81f92825255d5d8c1b992
dancan <dancan.morara@student.moringaschool.com>
2025-03-16 17:26:56 +0300

    scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase(); 
+}
+
+var setBestCustomer = () => bestCustomer = 'not bob';
+
+var overwriteBestCustomer = () => bestCustomer = 'maybe bob';
+
+const leastFavoriteCustomer = 'dan';
+
+var changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'job';
+
+
+
+
